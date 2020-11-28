f1296455dfb983af667eceebc634d43f3271a523
Zsolt Sz. Sztupák <mail@sztupy.hu>
2020-11-28 21:56:38 +0000

    Set minnimum viewport

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    if (screen.width < 450) {
+        var mvp = document.getElementById('vp');
+        mvp.setAttribute('content','user-scalable=no,width=710');
+    }
+}
