5d802e275181dc183758e9f860630b089fae1886
Zsolt Sz. Sztupák <mail@sztupy.hu>
2020-11-29 13:53:24 +0000

    Change responsive designn

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    if (screen.width < 450) {
-        var mvp = document.getElementById('vp');
-        mvp.setAttribute('content','user-scalable=no,width=750');
-    }
-}
